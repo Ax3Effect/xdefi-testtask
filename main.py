a5e2ba667cd35b46db8747c1988653f5d96c1b6a
Ax3 <ax3.mail@gmail.com>
2022-07-14 14:44:32 +0300

    add graphql w/ tokenlist

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from typing import Union
+import asyncio
 
 from fastapi import FastAPI
+from graphene import ObjectType, List, String, Int, Schema
+import graphene
+
+from starlette.applications import Starlette
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+import json
 
 app = FastAPI()
+
+class TokenType(ObjectType):
+    address = String()
+    symbol = String()
+    name = String()
+    decimals = Int()
+    chainId = String()
+    logoURI = String()
+    coingeckoId = String()
+
+
+class TokenQuery(ObjectType):
+    token_list = None
+    get_tokenlist = List(TokenType)
+    async def resolve_get_tokenlist(self, info):
+        with open("./tokenlists/ethereum.json") as courses:
+            token_list = json.load(courses)
+        return token_list
 
 
 @app.get("/")
@@ -13,3 +38,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+schema = graphene.Schema(query=TokenQuery, )
+app.mount("/graphql", GraphQLApp(schema, on_get=make_graphiql_handler()))
