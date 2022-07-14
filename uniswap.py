350f1d09cc1a9733d6c1c843030e0d044b02e3ce
Ax3 <ax3.mail@gmail.com>
2022-07-14 18:45:16 +0300

    processing pool pairs for destinations

--- uniswap.py
+++ uniswap.py
@@ -1,6 +1,8 @@
 from queries import UNISWAP_GET_POOLS
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
+
+import asyncio
 import requests
 import json
 
@@ -18,6 +20,28 @@ class UniswapConnect:
                 return token
         return None
     
+    async def process_pool_pairs(self, address):
+        data = await self.get_available_pools(address)
+
+        source_symbol = data['token']['symbol']
+        destinations = []
+        for pool in data['token']['whitelistPools']:
+            # ignore pairs with no liquidity
+            if int(pool['liquidity']) < 10000:
+                continue
+            
+            if source_symbol == pool['token0']['symbol']:
+                destination_data = pool['token1']
+            elif source_symbol == pool['token1']['symbol']:
+                destination_data = pool['token0']
+            
+            if not destination_data:
+                continue
+            
+            destinations.append(destination_data)
+        return destinations
+
+    
     async def send_uniswap_request(self, gql_query, variables={}):
         transport = AIOHTTPTransport(url=UNISWAP_SUBGRAPH_URL)
         client = Client(transport=transport, fetch_schema_from_transport=True)
@@ -31,7 +55,15 @@ class UniswapConnect:
         if token_data:
             query = gql(UNISWAP_GET_POOLS)
             token_id = str(token_data['address'].lower())
-            result = await self.send_uniswap_request(UNISWAP_GET_POOLS, {'tokenid': token_id})
+            result = (await self.send_uniswap_request(UNISWAP_GET_POOLS, {'tokenid': token_id}))
             return result
 
-#print(UniswapConnect().get_available_pools('0x6B175474E89094C44Da98b954EedeAC495271d0F'))
+async def test():
+    a = await UniswapConnect().process_pool_pairs('0x6B175474E89094C44Da98b954EedeAC495271d0F')
+    print(a)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test())
+    loop.close()
+
