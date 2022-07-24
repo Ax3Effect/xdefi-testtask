1acea15dad8805d0b6de5b402b2f863547333e72
Ax3 <ax3.mail@gmail.com>
2022-07-24 17:37:03 +0300

    front: send transaction

--- front/components/Profile.jsx
+++ front/components/Profile.jsx
@@ -1,4 +1,4 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useAccount, useConnect, useDisconnect, useSendTransaction  } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 
 export default function Profile() {
@@ -8,12 +8,34 @@ export default function Profile() {
   })
   const { disconnect } = useDisconnect()
 
+
+  const { data, isIdle, isError, isLoading, isSuccess, sendTransaction } =
+  useSendTransaction({
+    request: {
+      to: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+      value: 0, // 1 ETH,
+      nonce: 54,
+      data: '0x38ed17390000000000000000000000000000000000000000000000004563918244f4000000000000000000000000000000000000000000000000000043a77aabd007800000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000707a7e9606b0e1547d7f8401d92222430c3483990000000000000000000000000000000000000000000000000000000062dd4f850000000000000000000000000000000000000000000000000000000000000003000000000000000000000000514910771af9ca656af840dff83e8264ecf986ca000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000000000000000000000000d533a949740bb3306d119cc777fa900ba034cd52',
+      gasPrice: 20,
+      gasLimit: 200,
+    },
+  })
+
+
+
   if (isConnected)
     return (
       <div>
-        Connected to {address}
-        <button onClick={() => disconnect()}>Disconnect</button>
+        Connected to {address}<br></br>
+        <button class="mt-6 mb-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => disconnect()}>Disconnect</button>
+        <br></br>
+        <button disabled={isLoading} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => sendTransaction()}>Send test transaction</button> 
+        {isLoading && <div>Check Wallet</div>}
+        {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+        {isError && <div>Error sending transaction</div>}
       </div>
     )
-  return <button onClick={() => connect()}>Connect Wallet</button>
-}
+  return <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => connect()}>Connect Wallet</button>
+} 
+
+
--- front/components/Route.jsx
+++ front/components/Route.jsx
@@ -1,46 +1,5 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { CheckCircleIcon, ChevronRightIcon, MailIcon, ArrowRightIcon } from '@heroicons/react/solid'
-import { gql } from "@apollo/client";
-import client from "../apollo-client";
-
-const applications = [
-  {
-    applicant: {
-      name: 'Ricardo Cooper',
-      email: 'ricardo.cooper@example.com',
-      imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    date: '2020-01-07',
-    dateFull: 'January 7, 2020',
-    stage: 'Completed phone screening',
-    href: '#',
-  },
-  {
-    applicant: {
-      name: 'Kristen Ramos',
-      email: 'kristen.ramos@example.com',
-      imageUrl:
-        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    date: '2020-01-07',
-    dateFull: 'January 7, 2020',
-    stage: 'Completed phone screening',
-    href: '#',
-  },
-  {
-    applicant: {
-      name: 'Ted Fox',
-      email: 'ted.fox@example.com',
-      imageUrl:
-        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    date: '2020-01-07',
-    dateFull: 'January 7, 2020',
-    stage: 'Completed phone screening',
-    href: '#',
-  },
-]
 
 export default function Route(props) {
 
